8e07d4394d447888e828051795a786e0a175c90b
Xinmiao Yan <83053884+XinmiaoYan@users.noreply.github.com>
2025-04-10 22:22:33 -0500

    Update Figure 2.R

--- Figure 2.R
+++ Figure 2.R
@@ -40,3 +40,23 @@ Idents(obj) <- "level1_cell"
 levels(obj) <- c("B","Plasma", "T&NK", "T_Proliferating",'Myeloid','Mast', "pDCs","Epithelial", "Fibroblasts", "Endothelial")
 p2b = DimPlot(obj, reduction = 'umap',cols = my_color, pt.size = pt) #same as pS2b
 ggsave("01_clustering_p2b.pdf",plot=p2b, width = 6, height = 4.5)
+
+
+
+
+# Extract UMAP coordinates
+umap_coords <- Embeddings(obj, reduction = "umap")
+
+# Extract metadata
+metadata <- obj@meta.data
+table(metadata$seurat_clusters, metadata$seurat_clusters_new)
+
+identical(rownames(umap_coords), rownames(metadata))
+# Combine UMAP and metadata
+output_df <- cbind(UMAP_1 = umap_coords[, 1],
+                   UMAP_2 = umap_coords[, 2],
+                   metadata)
+
+head(output_df)
+# Write to a tab-separated file
+write.table(output_df, file = "01_umap_with_metadata.tsv", sep = "\t", quote = FALSE, row.names = TRUE)
